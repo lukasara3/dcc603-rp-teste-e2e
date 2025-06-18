33c1bb3fa42ef60f188c674852e5ab5660a7c979
lukasara3 <lucaoamsilva@gmail.com>
2025-06-18 10:37:48 -0300

    Implementa novos 3 testes de cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -16,6 +16,68 @@ describe('TODOMvc App', () => {
       .should('have.text', 'TP2 de Engenharia de Software'); 
   });
 
+  
+  it('Completa uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Sistemas Operacionais{enter}');
+    
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .should('have.class', 'completed');
+  })
+
+  it('Limpa tarefas completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP IHC{enter}')
+      .type('TP2 IA{enter}')
+      .type('Estudar prova SO{enter}');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .eq(0) 
+      .click();
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .eq(2) 
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1) 
+      .first()
+      .should('have.text', 'TP2 IA'); 
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Fazer teste cypress{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+      .first()
+      .dblclick();
+
+    cy.get('[data-cy=todos-list] > li.editing .edit')
+      .clear()
+      .type('Tarefa editada com sucesso{enter}');
+
+    cy.get('[data-cy=todos-list] > li')
+    .should('have.text', 'Tarefa editada com sucesso');
+
+    cy.get('[data-cy=todos-list]')
+    .children().should('have.length', 1);
+  });
+
   it('Insere e deleta uma tarefa', () => {
     cy.visit('');
 
